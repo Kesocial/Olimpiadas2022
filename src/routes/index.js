72bc3bc4299ea7a987aa005bf21e4436dba1f51f
Acevedo Kevin <87038684+Kesocial@users.noreply.github.com>
2022-09-21 19:20:43 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,18 @@
 const { Router } = require("express");
 const router = Router();
-const request = require('request');
+// const request = require('request');
 
 
 router.get("/", function(req, res) {
     let logueado = false;
     if (req.isAuthenticated()) logueado = true;
-    request.get(`https://olimpiadas2022eestn5.herokuapp.com/api/tematicas`, { json: true }, (err, response, body) => {
-        if (err) { return console.log(err); }
-        tematicas = body;
+    // request.get(`https://olimpiadas2022eestn5.herokuapp.com/api/tematicas`, { json: true }, (err, response, body) => {
+    //     if (err) { return console.log(err); }
+    //     tematicas = body;
 
-        res.render("landing/landing", { tematicas, logueado });
-    });
+    // });
+    const tematicas = [];
+    res.render("landing/landing", { tematicas, logueado });
 
 });
 
