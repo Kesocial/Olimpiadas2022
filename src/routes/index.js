1a9da437c47471972229e90e2060013c3eb4b51c
Acevedo Kevin <87038684+Kesocial@users.noreply.github.com>
2022-09-21 18:33:40 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,12 +7,12 @@ const request = require('request');
 router.get("/", function(req, res) {
     let logueado = false;
     if (req.isAuthenticated()) logueado = true;
-//     request.get(`http://${process.env.DOMAIN}:${process.env.PORT}/api/tematicas`, { json: true }, (err, response, body) => {
-//         if (err) { return console.log(err); }
-//         tematicas = body;
-//        });
-    tematicas = [];
+    request.get(`http://${process.env.DOMAIN}:${process.env.PORT}/api/tematicas`, { json: true }, (err, response, body) => {
+        if (err) { return console.log(err); }
+        tematicas = body;
+        
      res.render("landing/landing", {tematicas, logueado });
+       });
     
 });
 
